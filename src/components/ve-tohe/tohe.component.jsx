a59484015692dc771731f6a01afb4db75d70943e
Minh Nhat <nhokkudo143@gmail.com>
2026-06-18 10:31:38 +0700

    tohe

--- src/components/ve-tohe/tohe.component.jsx
+++ src/components/ve-tohe/tohe.component.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import './tohe.css.scss'
+import ButtonArrowRight from '../../camoms/button/button-arrow-right.camon copy';
 
 const ToheComponent = () => {
     useEffect(() => {
@@ -157,6 +158,190 @@ const ToheComponent = () => {
                                 </div>
                             </div>
                         </div>
+                    </div>
+
+                    {/* ability */}
+                    <div className="tohe-ability-container">
+                        <div className="tohe-ability-context">
+                            <div className="tohe-ability-top">
+                                <div className="tohe-ability-top-content">
+                                    <div className="tohe-ability-top-title-h1">
+                                        <h1 className='text-pink'>
+                                            Các cột mốc <br/>
+                                            phát triển <br/>
+                                            nổi bật
+                                        </h1>
+                                    </div>
+                                    <div className="tohe-ability-top-desc-list-content">
+                                        <ul className='tohe-ability-top-desc-list'>
+                                            <li>
+                                                29/09/2006 - Công ty Cổ phần Tòhe chính thức được thành lập.
+                                            </li>
+                                            <li>
+                                                09/2009 - Tòhe được công nhận là doanh nghiệp xã hội và nhận hỗ trợ khởi nghiệp từ CSIP và Hội đồng Anh Việt Nam.
+                                            </li>
+                                            <li>
+                                                2012 - Khai trương cửa hàng bán lẻ đầu tiên, phát triển mô hình sản phẩm sáng tạo từ tranh vẽ trẻ em.
+                                            </li>
+                                            <li>
+                                                2013 - Mở rộng hoạt động quốc tế với thị trường Đức và tham gia nhiều hội chợ, triển lãm sáng tạo tại Nhật Bản và Hàn Quốc.
+                                            </li>
+                                            <li>
+                                                2016 - Đồng sáng lập Phạm Thị Ngân được Diễn đàn Kinh tế Thế giới (WEF) vinh danh là Lãnh đạo trẻ toàn cầu.
+                                            </li>
+                                            <li>
+                                                2019 - Mở cửa hàng thứ hai tại Nhà Chung (Hà Nội), tiếp tục mở rộng hoạt động cộng đồng và hợp tác CSR/ESG.
+                                            </li>
+                                            <li>
+                                                Nay - Tòhe đã đồng hành cùng 400+ doanh nghiệp và tổ chức, triển khai hơn 3000 buổi học nghệ thuật và kết nối hơn 6000 trẻ em đặc biệt trên toàn quốc.
+                                            </li>
+                                        </ul>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="tohe-ability-active-title">
+                            <h1 className='tohe-ability-active-title-h1 text-blue'>
+                                Năng lực triển khai
+                            </h1>
+                            <div className="tohe-ability-active-content">
+                                <div className="background-content"></div>
+                                <div className="tohe-ability-active-content-item">
+                                    <div className="tohe-ability-active-content-item-box">
+                                        <div className="tohe-ability-active-content-item_item-top">
+                                            <div>
+                                                <h1 className='text-pink'>400+</h1>
+                                                <p className='p-top-ab font-uvn-b'>Đồng hành</p>
+                                                <p className='p-bottom-ab'>Doanh nghiệp và tổ chức</p>
+                                            </div>
+                                            <div>
+                                                <h1 className='text-pink'>3000</h1>
+                                                <p className='p-top-ab font-uvn-b'>Tổ chức</p>
+                                                <p className='p-bottom-ab'>Buổi học nghệ thuật và Workshop</p>
+                                            </div>
+                                            <div>
+                                                <h1 className='text-pink'>6000</h1>
+                                                <p className='p-top-ab font-uvn-b'>Kết nối</p>
+                                                <p className='p-bottom-ab'>Trẻ em đặc biệt</p>
+                                            </div>
+                                        </div>
+                                        <div className="tohe-ability-active-content-item_item-bottom">
+                                            <div>
+                                                <h1 className='text-pink'>50</h1>
+                                                <p className='p-top-ab font-uvn-b'>Triển khai</p>
+                                                <p className='p-bottom-ab'>Địa điểm trên nhiều tỉnh thành</p>
+                                            </div>
+                                            <div>
+                                                <h1 className='text-pink'>13</h1>
+                                                <p className='p-top-ab font-uvn-b'>Thực hiện</p>
+                                                <p className='p-bottom-ab'>Triển lãm nghệ thuật</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* action */}
+                    <div className="tohe-action-container">
+                        <div className="tohe-action-context">
+                            <div className="tohe-action-title">
+                                <h1 className='text-blue'>
+                                    CÁC HOẠT ĐỘNG TIÊU BIỂU CỦA <span className='text-pink'>TÒHE</span>
+                                </h1>
+                                <p>
+                                    Chung tôi chuyên tổ chức các sân chơi sáng tạo nghệ thuật tạo hình phi lợi nhuận hướng tới đối tượng là trẻ em có <br/>
+                                    hoàn cảnh, nhu cầu đặc biệt, giúp các em tự tin, tự do bộc lộ và phát triển khả năng sáng tạo của mình.
+                                </p>
+                            </div>
+                            <div className="tohe-action-title-box">
+                                <div className="tohe-action-box-content">
+                                    <div className="tohe-action-box-item">
+                                        <div className="tohe-action-box-item_item">
+                                            <img src="/img/tohe/ve tohe/icon-v1.png" alt="" className='tohe-action-box-item-image'/>
+                                            <p className='tohe-action-box-item-text-top font-uvn-b'>
+                                                Sân chơi <br/> nghệ thuật
+                                            </p>
+                                            <p className='tohe-action-box-item-text-bottom'>
+                                                Hoạt động hội họa sáng tạo dành cho trẻ em đặc biệt. Giúp trẻ phát triển khả năng biểu đạt và kết nối xã hội.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="tohe-action-box-content">
+                                    <div className="tohe-action-box-item">
+                                        <div className="tohe-action-box-item_item">
+                                            <img src="/img/tohe/ve tohe/icon-v2.png" alt="" className='tohe-action-box-item-image'/>
+                                            <p className='tohe-action-box-item-text-top font-uvn-b'>
+                                                Art Coaching
+                                            </p>
+                                            <p className='tohe-action-box-item-text-bottom'>
+                                                Chương trình nghệ thuật chuyên sâu dành cho trẻ có năng khiếu hội họa. Hỗ trợ phát triển khả năng sáng tác và sáng tạo cá nhân.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="tohe-action-box-content">
+                                    <div className="tohe-action-box-item">
+                                        <div className="tohe-action-box-item_item">
+                                            <img src="/img/tohe/ve tohe/icon-v3.png" alt="" className='tohe-action-box-item-image'/>
+                                            <p className='tohe-action-box-item-text-top font-uvn-b'>
+                                                Xưởng đào <br/> tạo nghề
+                                            </p>
+                                            <p className='tohe-action-box-item-text-bottom'>
+                                                Hoạt động đào tạo nghề thủ công cho trẻ tự kỷ. Giúp trẻ tăng khả năng hòa nhập và định hướng nghề nghiệp.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="tohe-action-box-content">
+                                    <div className="tohe-action-box-item">
+                                        <div className="tohe-action-box-item_item">
+                                            <img src="/img/tohe/ve tohe/icon-v4.png" alt="" className='tohe-action-box-item-image'/>
+                                            <p className='tohe-action-box-item-text-top font-uvn-b'>
+                                                Tòhe Bank
+                                            </p>
+                                            <p className='tohe-action-box-item-text-bottom'>
+                                                Ngân hàng tranh lưu trữ tác phẩm của trẻ em trên khắp Việt Nam. Giúp lan tỏa sáng tạo và tạo thêm thu nhập cho các em.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="tohe-action-box-content">
+                                    <div className="tohe-action-box-item">
+                                        <div className="tohe-action-box-item_item">
+                                            <img src="/img/tohe/ve tohe/icon-v5.png" alt="" className='tohe-action-box-item-image'/>
+                                            <p className='tohe-action-box-item-text-top font-uvn-b'>
+                                                Sân chơi<br/> trở thành nghệ sĩ bé
+                                            </p>
+                                            <p className='tohe-action-box-item-text-bottom'>
+                                                Chương trình kết nối trẻ em với các hoạt động nghệ thuật của Tòhe. Giúp trẻ được công nhận và phát triển khả năng sáng tạo.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="tohe-action-box-content">
+                                    <div className="tohe-action-box-item">
+                                        <div className="tohe-action-box-item_item">
+                                            <img src="/img/tohe/ve tohe/icon-v6.png" alt="" className='tohe-action-box-item-image'/>
+                                            <p className='tohe-action-box-item-text-top font-uvn-b'>
+                                                Triển lãm nâng <br/> cao nhận thức
+                                            </p>
+                                            <p className='tohe-action-box-item-text-bottom'>
+                                                Chuỗi triển lãm cộng đồng về trẻ tự kỷ và người có nhu cầu đặc biệt thông qua nghệ thuật. Giúp lan tỏa nhận thức và kết nối cộng đồng.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="tohe-button-next">
+                        <ButtonArrowRight title={'Xem hình thức đồng hành'} />
                     </div>
 
                 </div>
